245cbdad7b41bb6264757978e938615419a73d9a
chengzhinei <chengxue5yuan@163.com>
2024-08-01 19:19:14 +0800

    fix ut

--- src/hash_builder/mod.rs
+++ src/hash_builder/mod.rs
@@ -519,7 +519,7 @@ mod tests {
 
         let update = updates.get(&Nibbles::from_nibbles_unchecked(hex!("01"))).unwrap();
         assert_eq!(update.state_mask, TrieMask::new(0b1111)); // 1st nibble: 0, 1, 2, 3
-        assert_eq!(update.tree_mask, TrieMask::new(6));
+        assert_eq!(update.tree_mask, TrieMask::new(6)); // in the 1st nibble, the ones with 1 and 2 are branches. value:0000000000000110
         assert_eq!(update.hash_mask, TrieMask::new(6)); // in the 1st nibble, the ones with 1 and 2 are branches with `hashes`
         assert_eq!(update.hashes.len(), 2); // calculated while the builder is running
 
@@ -621,6 +621,6 @@ mod tests {
 
         let _root = hb.root();
         let (_, updates) = hb.split();
-        assert!(updates.len() > 0);
+        assert!(!updates.is_empty());
     }
 }
